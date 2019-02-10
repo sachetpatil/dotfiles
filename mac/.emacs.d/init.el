d57be6981771b8b013f453c9efbc31084ad9d937
Sachet Patil <sachet.patil@1010data.com>
2019-02-10 17:59:01 -0500

    updating init file

--- mac/.emacs.d/init.el
+++ mac/.emacs.d/init.el
@@ -6,6 +6,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Xterm-color-names-bright
+   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"])
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
@@ -18,7 +20,7 @@
  '(custom-enabled-themes (quote (gruvbox-dark-medium)))
  '(custom-safe-themes
    (quote
-    ("fd3c7bd752f48dcb7efa5f852ef858c425b1c397b73851ff8816c0580eab92f1" "7f89ec3c988c398b88f7304a75ed225eaac64efa8df3638c815acc563dfd3b55" "a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c" "e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" "2a9039b093df61e4517302f40ebaf2d3e95215cb2f9684c8c1a446659ee226b9" "c280a47cb66453f297553efcd0152666bdaab65ef4e8dfc498973f81ec1b60bb" "01c3b475bc176a1eb66ad7f2513523f59fb131c58132b975b254624198c8fd41" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" default)))
+    ("a94f1a015878c5f00afab321e4fef124b2fc3b823c8ddd89d360d710fc2bddfc" "0cd56f8cd78d12fc6ead32915e1c4963ba2039890700458c13e12038ec40f6f5" "158013ec40a6e2844dbda340dbabda6e179a53e0aea04a4d383d69c329fba6e6" "228c0559991fb3af427a6fa4f3a3ad51f905e20f481c697c6ca978c5683ebf43" "b563a87aa29096e0b2e38889f7a5e3babde9982262181b65de9ce8b78e9324d5" "73a13a70fd111a6cd47f3d4be2260b1e4b717dbf635a9caee6442c949fad41cd" "c616e584f7268aa3b63d08045a912b50863a34e7ea83e35fcab8537b75741956" "721bb3cb432bb6be7c58be27d583814e9c56806c06b4077797074b009f322509" "01e067188b0b53325fc0a1c6e06643d7e52bc16b6653de2926a480861ad5aa78" "9b1c580339183a8661a84f5864a6c363260c80136bd20ac9f00d7e1d662e936a" "d21135150e22e58f8c656ec04530872831baebf5a1c3688030d119c114233c24" "8d5f22f7dfd3b2e4fc2f2da46ee71065a9474d0ac726b98f647bc3c7e39f2819" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" "3eb93cd9a0da0f3e86b5d932ac0e3b5f0f50de7a0b805d4eb1f67782e9eb67a4" "b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" "fd3c7bd752f48dcb7efa5f852ef858c425b1c397b73851ff8816c0580eab92f1" "7f89ec3c988c398b88f7304a75ed225eaac64efa8df3638c815acc563dfd3b55" "a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c" "e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" "2a9039b093df61e4517302f40ebaf2d3e95215cb2f9684c8c1a446659ee226b9" "c280a47cb66453f297553efcd0152666bdaab65ef4e8dfc498973f81ec1b60bb" "01c3b475bc176a1eb66ad7f2513523f59fb131c58132b975b254624198c8fd41" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" default)))
  '(fci-rule-color "#eee8d5")
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-symbol-colors
@@ -42,19 +44,23 @@
     ("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342")))
  '(hl-fg-colors
    (quote
-    ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
+    ("#fdf6e3" "#Fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
  '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
  '(magit-diff-use-overlays nil)
  '(menu-bar-mode t)
  '(nrepl-message-colors
    (quote
     ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(org-agenda-files (list org-directory))
+ '(org-directory "~/org")
+ '(org-startup-truncated nil)
  '(package-selected-packages
    (quote
-    (gruvbox-theme evil-org evil-surround org-send-ebook solarized-theme dracula-theme evil)))
+    (airline-themes powerline smart-mode-line-powerline-theme jedi pipenv org-bullets ## gruvbox-theme evil-surround org-send-ebook solarized-theme dracula-theme evil)))
  '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#282828")))
  '(pos-tip-background-color "#eee8d5")
  '(pos-tip-foreground-color "#586e75")
+ '(scroll-bar-mode nil)
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
  '(term-default-bg-color "#fdf6e3")
  '(term-default-fg-color "#657b83")
@@ -82,14 +88,12 @@
      (320 . "#27c19460bb87")
      (340 . "#26f38ff5c72c")
      (360 . "#268bd2"))))
- '(vc-annotate-very-old-color nil)
+ '(vc-annttate-very-old-color nil)
  '(weechat-color-list
    (quote
     (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
  '(xterm-color-names
-   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
- '(xterm-color-names-bright
-   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
+   ["#eee8d5" "#Dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -109,3 +113,71 @@
 ;; Adding a load-path to load k syntax highlighting
 (add-to-list 'load-path "~/.emacs.d/syntax/")
 (require 'k-mode)
+
+;; Adding Org mode bullets
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; Adding TODO sequences
+(setq org-todo-keywords
+  '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
+;; Adding colors to todo keywords
+(setq org-todo-keyword-faces
+ '(("IN-PROGRESS" . "#2aa198") ("CANCELED" . "#859900") ("DONE" . "#859900"))
+ )
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+
+
+;; Adding Tabs stuff
+(setq-default indent-tabs-mode nil)
+(setq tab-width 4) ; or any other preferred value
+
+;; Saving History
+(savehist-mode 1)
+
+;; Poweline
+(require 'powerline)
+(powerline-default-theme)
+(require 'airline-themes)
+(load-theme 'airline-doom-molokai)
+
+;; Spellcheck stuff
+;; Define aspell as the spellchecking engine.
+(setq-default ispell-program-name "aspell")
+
+;; Set the default language dictionary.
+(ispell-change-dictionary "en_US" t)
+
+;; Prevent Flyspell from printing a log for every checked word in a buffer to
+;; avoid performance impacts.
+(setq flyspell-issue-message-flag nil)
+
+(add-hook 'text-mode-hook 'flyspell-mode)       ; Activate Flyspell by default
+                                                ; in text modes..
+(add-hook 'org-mode-hook 'turn-on-flyspell)     ; Activate Flyspell by default
+                                                ; in org-mode.
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)  ; Spellcheck comments in
+                                                ; programming modes.
+
+;; Prevent Flycheck from checking code blocks in org-mode.
+(add-to-list 'ispell-skip-region-alist '("#\\+begin_src". "#\\+end_src"))
+
+;; Improve Flyspell keybindings.
+(global-set-key (kbd "C-c w") 'ispell-word)
+(global-set-key (kbd "C-c p") 'flyspell-check-previous-highlighted-word)
+(defun flyspell-check-next-highlighted-word ()
+  "Custom function to spell check next highlighted word"
+  (interactive)
+  (flyspell-goto-next-error)
+  (ispell-word))
+(global-set-key (kbd "C-c n") 'flyspell-check-next-highlighted-word)
+
+(eval-after-load "flyspell"
+  '(progn
+     (define-key flyspell-mouse-map [C-down-mouse-1] #'flyspell-correct-word)
+     ))
+
+;; Match Parenthesis
+(show-paren-mode 1)
